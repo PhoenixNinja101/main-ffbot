290c5ace7236d7e089e63238db5e5320ac24ac38
fergusc20 <rapuru.revati@gmail.com>
2020-12-09 12:28:16 -0500

    Version 1.01

--- src/main/java/discordAPI/ffnetEvent.java
+++ src/main/java/discordAPI/ffnetEvent.java
@@ -16,21 +16,23 @@ public class ffnetEvent extends ListenerAdapter {
 				String ffID = " ";
 				if(messageSent.length == 2) {
 					ffID = messageSent[1];
+					
+					if (!ffID.equals(" ") && g.canFindFF(ffID)) {
+						event.getChannel().sendMessage(g.getTitle(ffID)).queue();	
+						event.getChannel().sendMessage(g.getAuthor(ffID)).queue();
+						event.getChannel().sendMessage(g.getDesc(ffID)).queue();
+						event.getChannel().sendMessage(g.getByLine(ffID)).queue();
+						
+					}
+					else {
+						event.getChannel().sendMessage("I cannot find a fanfiction with that ID. Please try again.").queue();;
+					}
 				}
 				else {
 					event.getChannel().sendMessage("!linkffn (ffID) | Please use the command in this way and don't add multiple spaces.").queue();	
 				}
 				
-				if (!ffID.equals(" ") && g.canFindFF(ffID)) {
-					event.getChannel().sendMessage(g.getTitle(ffID)).queue();	
-					event.getChannel().sendMessage(g.getAuthor(ffID)).queue();
-					event.getChannel().sendMessage(g.getDesc(ffID)).queue();
-					event.getChannel().sendMessage(g.getByLine(ffID)).queue();
-					
-				}
-				else {
-					event.getChannel().sendMessage("I cannot find a fanfiction with that ID. Please try again.").queue();;
-				}
+				
 				
 				
 				
