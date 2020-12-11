1a08c1f27a0cd4bf650f2a672077a44aa0f9cd16
fergusc20 <rapuru.revati@gmail.com>
2020-12-11 18:21:41 -0500

    Version 1.9

--- src/main/java/discordAPI/ffnetEvent.java
+++ src/main/java/discordAPI/ffnetEvent.java
@@ -22,16 +22,13 @@ public class ffnetEvent extends ListenerAdapter {
 					
 					if (!ffID.equals(" ") && g.canFindFF(ffID)) {
 						EmbedBuilder eb = new EmbedBuilder();
-						
-						eb.setTitle("Hello", "https://google.com");
-						eb.setColor(Color.red);
-						eb.setDescription("Text");
-						eb.addField("Title blah blah blah blah", "Text blah blah blah", true);
-						eb.addBlankField(false);
-						eb.setAuthor("name", null, "https://github.com/zekroTJA/DiscordBot/blob/master/.websrc/zekroBot_Logo_-_round_small.png");
-						eb.setFooter("Text", "https://github.com/zekroTJA/DiscordBot/blob/master/.websrc/zekroBot_Logo_-_round_small.png");
-						eb.setImage("https://github.com/zekroTJA/DiscordBot/blob/master/.websrc/logo%20-%20title.png"); 	
-						eb.setThumbnail("https://github.com/zekroTJA/DiscordBot/blob/master/.websrc/logo%20-%20title.png");
+						eb.setTitle("Amalgum – Lockhart's Folly", "https://www.fanfiction.net/s/11913447/");
+					    eb.setDescription("By: [tkepnr](https://www.fanfiction.net/u/5362799/tkepner)");
+					    eb.setColor(new Color(13390553));
+					    eb.setFooter("Rated: Fiction T - English - Adventure/Humor - Harry P., Hermione G., Gilderoy L., Bellatrix L. - Chapters: 31 - Words: 192,977 - Reviews: 1,964 - Favs: 6,555 - Follows: 3,918 - Updated: Feb 20, 2017 - Published: Apr 24, 2016 - Status: Complete - id: 1191","https://i.pinimg.com/474x/ce/45/5e/ce455e6bcf90fc1826fb4102f1d247d9.jpg");
+//debug this line					    eb.setThumbnail("https://fanfiction.net/image/1356205/75/");
+					    eb.addField("_______", "Death wants free of its Master and proposes sending Harry back in time to avoid the unnecessary deaths in fighting Voldemort. Harry readily accepts, thinking he'll start anew as a Firstie. Instead, Harry's soul, magic, and memories end up at the beginning of Second Year — in GILDEROY LOCKHART!", false);
+					   
 						
 						event.getChannel().sendMessage(eb.build()).queue();
 					}
