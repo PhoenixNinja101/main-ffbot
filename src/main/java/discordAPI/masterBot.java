441f6c1c95a9e5d875d60e6dee176596d1d4e45d
fergusc20 <74146775+fergusc20@users.noreply.github.com>
2020-12-13 17:13:12 -0500

    Update masterBot.java

--- src/main/java/discordAPI/masterBot.java
+++ src/main/java/discordAPI/masterBot.java
@@ -8,7 +8,7 @@ import net.dv8tion.jda.api.JDABuilder;
 public class masterBot {
 	
 	public static void main (String [] args) throws LoginException{
-		JDABuilder jda = JDABuilder.createDefault("Nzc2MTkzMTU4ODU3ODgzNjk5.X6xUNg.1d9Ux6OWmWG-u9_06Pab_1Xurcs");
+		JDABuilder jda = JDABuilder.createDefault(token);
 	
 		jda.addEventListeners(new ffnetEvent());
 		
